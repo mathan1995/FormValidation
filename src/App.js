1b7428236cfdfc209a7314340b8bb9f4b9110798
mathan1995 <mathanganjeya@gmail.com>
2019-06-27 16:42:10 +0530

    Added Only Number validation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import "./App.css";
+// import "./App.css";
 import Main from "./FormMain";
 import UserForm from "./formUi";
 import Welcome from "./welcome";
 import EmptyValidate from "./EmptyField";
+import EmptyValidate2 from "./emptyValidate2";
 
 class App extends Component {
   render() {
-    return <EmptyValidate />;
+    return <EmptyValidate2 />;
   }
 }
 
---
+++ src/onlyNumberValidate.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class EmptyValidate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputs: [
+        { name: "price", value: 0, error: "" },
+        { name: "downP", value: 0, error: "" },
+        { name: "term", value: 0, error: "" },
+        { name: "interest", value: 0, error: "" }
+      ]
+    };
+  }
+
+  handleInputChange = (idx, event) => {
+    const target = event.target;
+    const name = target.name;
+    let error = "";
+
+    if (isNaN(target.value)) {
+      error = `${name} field can only be number`;
+    }
+
+    if (!target.value) {
+      error = `${name} field cannot be empty`;
+    }
+
+    this.state.inputs[idx] = {
+      ...this.state.inputs[idx],
+      value: target.value,
+      error
+    };
+
+    this.setState({
+      inputs: [...this.state.inputs]
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        {this.state.inputs.map((input, idx) => (
+          <div>
+            <label htmlFor="">{input.name}</label>
+            <input
+              type="text"
+              value={input.value}
+              onChange={e => this.handleInputChange(idx, e)}
+            />
+            {input.error && <span>{input.error}</span>}
+          </div>
+        ))}
+      </form>
+    );
+  }
+}
+export default EmptyValidate;
