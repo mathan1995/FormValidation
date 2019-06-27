29c51af18252e1eebd9e67f43cb342efbef58d35
mathan1995 <mathanganjeya@gmail.com>
2019-06-27 16:34:18 +0530

    Added Empty FIeld Validation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import Main from "./FormMain";
 import UserForm from "./formUi";
 import Welcome from "./welcome";
+import EmptyValidate from "./EmptyField";
 
 class App extends Component {
   render() {
-    return <Main />;
+    return <EmptyValidate />;
   }
 }
 
